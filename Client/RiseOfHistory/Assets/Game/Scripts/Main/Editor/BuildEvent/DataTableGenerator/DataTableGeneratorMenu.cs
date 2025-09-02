f033bc9a585322dcae3c73d67b8e5696dccd0660
彭武阳 <94458936@qq.com>
2025-09-02 09:49:27 +0800

    + 生成数据工具

---
+++ Client/RiseOfHistory/Assets/Game/Scripts/Main/Editor/BuildEvent/DataTableGenerator/DataTableGeneratorMenu.cs
@@ -0,0 +1,36 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2021 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+using Game.Scripts.Main.Runtime.Procedure;
+using GameFramework;
+using UnityEditor;
+using UnityEngine;
+
+namespace StarForce.Editor.DataTableTools
+{
+    public sealed class DataTableGeneratorMenu
+    {
+        [MenuItem("Rise of History/Generate DataTables")]
+        private static void GenerateDataTables()
+        {
+            foreach (string dataTableName in ProcedurePreload.DataTableNames)
+            {
+                DataTableProcessor dataTableProcessor = DataTableGenerator.CreateDataTableProcessor(dataTableName);
+                if (!DataTableGenerator.CheckRawData(dataTableProcessor, dataTableName))
+                {
+                    Debug.LogError(Utility.Text.Format("Check raw data failure. DataTableName='{0}'", dataTableName));
+                    break;
+                }
+
+                DataTableGenerator.GenerateDataFile(dataTableProcessor, dataTableName);
+                DataTableGenerator.GenerateCodeFile(dataTableProcessor, dataTableName);
+            }
+
+            AssetDatabase.Refresh();
+        }
+    }
+}
