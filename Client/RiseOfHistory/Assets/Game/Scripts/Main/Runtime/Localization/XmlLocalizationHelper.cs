0e3aab9181e9d42a4e0dd3e14eb3b6f965984998
彭曄恩 <94458936@qq.com>
2025-09-03 22:19:31 +0800

    + 类重命名

--- Client/RiseOfHistory/Assets/Game/Scripts/Main/Runtime/Localization/XmlLocalizationHelper.cs
+++ Client/RiseOfHistory/Assets/Game/Scripts/Main/Runtime/Localization/XmlLocalizationHelper.cs
@@ -1,16 +1,9 @@
-﻿//------------------------------------------------------------
-// Game Framework
-// Copyright © 2013-2021 Jiang Yin. All rights reserved.
-// Homepage: https://gameframework.cn/
-// Feedback: mailto:ellan@gameframework.cn
-//------------------------------------------------------------
-
-using GameFramework.Localization;
-using System;
+﻿using System;
 using System.Xml;
+using GameFramework.Localization;
 using UnityGameFramework.Runtime;
 
-namespace RiseOfHistory
+namespace Game.Scripts.Main.Runtime.Localization
 {
     /// <summary>
     /// XML 格式的本地化辅助器。
@@ -20,6 +13,7 @@ namespace RiseOfHistory
         /// <summary>
         /// 解析字典。
         /// </summary>
+        /// <param name="localizationManager">本地化管理器</param>
         /// <param name="dictionaryString">要解析的字典字符串。</param>
         /// <param name="userData">用户自定义数据。</param>
         /// <returns>是否解析字典成功。</returns>
@@ -27,36 +21,36 @@ namespace RiseOfHistory
         {
             try
             {
-                string currentLanguage = Game.Scripts.Main.Runtime.Base.GameEntry.Localization.Language.ToString();
-                XmlDocument xmlDocument = new XmlDocument();
+                var currentLanguage = Base.GameEntry.Localization.Language.ToString();
+                var xmlDocument = new XmlDocument();
                 xmlDocument.LoadXml(dictionaryString);
-                XmlNode xmlRoot = xmlDocument.SelectSingleNode("Dictionaries");
-                XmlNodeList xmlNodeDictionaryList = xmlRoot.ChildNodes;
-                for (int i = 0; i < xmlNodeDictionaryList.Count; i++)
+                var xmlRoot = xmlDocument.SelectSingleNode("Dictionaries");
+                var xmlNodeDictionaryList = xmlRoot.ChildNodes;
+                for (var i = 0; i < xmlNodeDictionaryList.Count; i++)
                 {
-                    XmlNode xmlNodeDictionary = xmlNodeDictionaryList.Item(i);
+                    var xmlNodeDictionary = xmlNodeDictionaryList.Item(i);
                     if (xmlNodeDictionary.Name != "Dictionary")
                     {
                         continue;
                     }
 
-                    string language = xmlNodeDictionary.Attributes.GetNamedItem("Language").Value;
+                    var language = xmlNodeDictionary.Attributes.GetNamedItem("Language").Value;
                     if (language != currentLanguage)
                     {
                         continue;
                     }
 
-                    XmlNodeList xmlNodeStringList = xmlNodeDictionary.ChildNodes;
-                    for (int j = 0; j < xmlNodeStringList.Count; j++)
+                    var xmlNodeStringList = xmlNodeDictionary.ChildNodes;
+                    for (var j = 0; j < xmlNodeStringList.Count; j++)
                     {
-                        XmlNode xmlNodeString = xmlNodeStringList.Item(j);
+                        var xmlNodeString = xmlNodeStringList.Item(j);
                         if (xmlNodeString.Name != "String")
                         {
                             continue;
                         }
 
-                        string key = xmlNodeString.Attributes.GetNamedItem("Key").Value;
-                        string value = xmlNodeString.Attributes.GetNamedItem("Value").Value;
+                        var key = xmlNodeString.Attributes.GetNamedItem("Key").Value;
+                        var value = xmlNodeString.Attributes.GetNamedItem("Value").Value;
                         if (!localizationManager.AddRawString(key, value))
                         {
                             Log.Warning("Can not add raw string with key '{0}' which may be invalid or duplicate.", key);
