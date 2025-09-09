26cf4e83c04df63ce69d42d0ffd5e4e894b49deb
彭武阳 <94458936@qq.com>
2025-09-09 14:21:48 +0800

    阵营

---
+++ Client/RiseOfHistory/Assets/Game/Scripts/Main/Runtime/GameData/User/MoralityType.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Main.Runtime.GameData.User
+{
+    public enum MoralityType
+    {
+        [InspectorName("仁德")] Benevolence = (0x01 << 3) + 0x07,
+        [InspectorName("中道")] Moderation = (0x01 << 4) + 0x07,
+        [InspectorName("诡诈")] Craftiness = (0x01 << 5) + 0x07,
+    }
+}
---
+++ Client/RiseOfHistory/Assets/Game/Scripts/Main/Runtime/GameData/User/RulesType.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Main.Runtime.GameData.User
+{
+    public enum RulesType
+    {
+        [InspectorName("守序")] Lawful = (0x01 << 0) + 0x38,
+        [InspectorName("逍遥")] Carefree = (0x01 << 1) + 0x38,
+        [InspectorName("混乱")] Chaos = (0x01 << 2) + 0x38,
+    }
+}
+ 
