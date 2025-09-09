e1dede8ea57b307499d9b2b2699a0bc5669872a8
彭武阳 <94458936@qq.com>
2025-09-09 17:33:19 +0800

    + 重命名

---
+++ Client/RiseOfHistory/Assets/Game/Scripts/Main/Runtime/UI/UICreate/SelectAttributeForm.cs
@@ -0,0 +1,42 @@
+﻿using Game.Scripts.Main.Runtime.Procedure.Scene;
+using Game.Scripts.Main.Runtime.UI.UICommon;
+using UnityGameFramework.Runtime;
+
+namespace Game.Scripts.Main.Runtime.UI.UICreate
+{
+    public class SelectAttributeForm : UGuiForm
+    {
+        private ProcedureCreate procedureCreate = null;
+
+        public void OnReturnButtonClick()
+        {
+            procedureCreate.RemoveUIForm(UIFormId.SelectAttributeForm);
+        }
+
+        public void OnEnterButtonClick()
+        {
+            procedureCreate.OpenUIForm(UIFormId.SelectSpiritualForm);
+        }
+
+        protected override void OnOpen(object userData)
+        {
+            base.OnOpen(userData);
+
+            procedureCreate = (ProcedureCreate)GetCurrentProcedure();
+
+            if (procedureCreate == null)
+            {
+                Log.Warning("ProcedureCreate is invalid when open SelectAvatarForm.");
+            }
+
+
+        }
+
+        protected override void OnClose(bool isShutdown, object userData)
+        {
+            procedureCreate = null;
+
+            base.OnClose(isShutdown, userData);
+        }
+    }
+}
