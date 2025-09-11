178eb737775d763e00f70227c8b27f7ec77b1e96
彭武阳 <94458936@qq.com>
2025-09-11 12:33:10 +0800

    + 灵根界面

--- Client/RiseOfHistory/Assets/Game/Scripts/Main/Runtime/UI/UICreate/Display/TalentDisplay.cs
+++ Client/RiseOfHistory/Assets/Game/Scripts/Main/Runtime/UI/UICreate/Display/TalentDisplay.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game.Scripts.Main.Runtime.UI.UICreate.Display
 {
     public class TalentDisplay : MonoBehaviour
     {
+        [SerializeField] private Text talentDescription;
         public void Refresh()
         {
              
