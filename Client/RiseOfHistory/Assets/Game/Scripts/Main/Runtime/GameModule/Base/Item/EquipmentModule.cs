4f5b31b6f7aaaf01653cd1d6cd88c6ec2158274e
彭武阳 <94458936@qq.com>
2025-09-12 16:24:02 +0800

    + 增加模块

---
+++ Client/RiseOfHistory/Assets/Game/Scripts/Main/Runtime/GameModule/Base/Item/EquipmentModule.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Main.Runtime.GameModule.Base.Item
+{
+    [Module]
+    public class EquipmentModule : BaseModule
+    {
+        
+    }
+}
---
+++ Client/RiseOfHistory/Assets/Game/Scripts/Main/Runtime/GameModule/Base/Plot/ChapterModule.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Main.Runtime.GameModule.Base.Plot
+{
+    [Module]
+    public class ChapterModule : BaseModule
+    {
+        
+    }
+}
