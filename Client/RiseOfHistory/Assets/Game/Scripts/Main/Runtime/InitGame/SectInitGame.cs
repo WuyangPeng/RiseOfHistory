788f1337703f825bbaec395aef29d96a6e1b8bf8
彭曄恩 <94458936@qq.com>
2025-09-14 11:15:27 +0800

    + 创建角色

--- Client/RiseOfHistory/Assets/Game/Scripts/Main/Runtime/InitGame/SectInitGame.cs
+++ Client/RiseOfHistory/Assets/Game/Scripts/Main/Runtime/InitGame/SectInitGame.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using Game.Scripts.Main.Runtime.Base;
+using Game.Scripts.Main.Runtime.GameModule.User;
+using Game.Scripts.Main.Runtime.GameModule.World;
+using UnityEngine;
 
 namespace Game.Scripts.Main.Runtime.InitGame
 {
     public class SectInitGame : InitGameBase
     {
+        private readonly UserModule userModule = GameEntry.ModuleComponent.GetModule<UserModule>();
+        private readonly NpcModule npcModule = GameEntry.ModuleComponent.GetModule<NpcModule>();
+
         public override void InitGame()
         {
 
